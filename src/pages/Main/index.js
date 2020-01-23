15bdb1b6bf63a05c286bf0f902b6593afec6a1c0
Evaldo Klock <neto.nemesis@gmail.com>
2020-01-23 08:40:13 -0400

    init search component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container } from './styles';
 import HeaderReduc from '../../components/HeaderReduc';
 import NavBar from '../../components/NavBar';
+import SearchInput from '../../components/SearchInput';
 
 export default function Main() {
   return (
     <Container>
       <HeaderReduc>
-        <NavBar></NavBar>
+        <NavBar />
+        <SearchInput />
       </HeaderReduc>
     </Container>
   );
